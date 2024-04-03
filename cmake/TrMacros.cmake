8b4e9bb4da6f5b42a61383d115540a5b80acefa6
herbyuan <171908804@qq.com>
2024-04-03 19:00:21 +0800

    2

--- cmake/TrMacros.cmake
+++ cmake/TrMacros.cmake
@@ -221,7 +221,7 @@ macro(tr_add_external_auto_library ID DIRNAME LIBNAME)
                 ${${ID}_EXT_PROJ_CMAKE_ARGS}
                 ${_TAEAL_ARG_CMAKE_ARGS}
             BUILD_BYPRODUCTS
-                ${ID}_BUILD_LIBS)
+                ${${ID}_BUILD_LIBS})
 
         set_property(TARGET ${${ID}_UPSTREAM_TARGET} PROPERTY FOLDER "${TR_THIRD_PARTY_DIR_NAME}")
 
@@ -240,7 +240,7 @@ macro(tr_add_external_auto_library ID DIRNAME LIBNAME)
 
         target_link_libraries(${_TAEAL_ARG_TARGET}
             INTERFACE
-                ${ID}_BUILD_LIBS)
+                ${${ID}_BUILD_LIBS})
 
         if(${ID}_UPSTREAM_TARGET)
             add_dependencies(${_TAEAL_ARG_TARGET} ${${ID}_UPSTREAM_TARGET})
